2d392753900f7491a601b3b1c07abb0051e05b2c
Ross Churchley <ross@rosschurchley.com>
2020-03-29 15:25:32 -0700

    Tweak colours

--- themes/blink/glimmerized.js
+++ themes/blink/glimmerized.js
@@ -1,24 +1,24 @@
-black       = '#140E25';
-red         = '#FF80C3';
-green       = '#81E4B7';
-yellow      = '#FFD666';
-blue        = '#80BFFF';
-magenta     = '#B699FF';
-cyan        = '#BCB9C6';
-white       = '#F6F5FA';
-lightBlack  = '#262136';
-lightRed    = '#F90688';
-lightGreen  = '#12A160';
-lightYellow = '#FFBB00';
-lightBlue   = '#0680F9';
-lightMagenta= '#714DCB';
-lightCyan   = '#504C5D';
-lightWhite  = '#FCFBFE';
+black       = '#0d0816';
+red         = '#ff80ca';
+green       = '#7dd9af';
+yellow      = '#ffe18f';
+blue        = '#67b0fe';
+magenta     = '#b69afe';
+cyan        = '#7c778d';
+white       = '#bbb7c8';
+lightBlack  = '#413c53';
+lightRed    = '';
+lightGreen  = '';
+lightYellow = '';
+lightBlue   = '';
+lightMagenta= '';
+lightCyan   = '#413c53';
+lightWhite  = '#fcfbfe';
 
 t.prefs_.set('color-palette-overrides',
                  [ black, red, green, yellow, blue, magenta, cyan, white,
                    lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite ]);
 
 t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
-t.prefs_.set('foreground-color', '#FCFBFE');
-t.prefs_.set('background-color', '#140E25');
+t.prefs_.set('foreground-color', '#fcfbfe');
+t.prefs_.set('background-color', '#0d0816');
